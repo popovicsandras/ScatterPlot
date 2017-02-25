d1d3315d281aeff314d713720c2e6fddf0564e5b
Andras Popovics <popovics@ndras.hu>
2017-02-25 01:27:25 +0000

    Chart.js

--- src/client/client.jsx
+++ src/client/client.jsx
@@ -1,31 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { ScatterChart, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Scatter } from 'recharts';
+import Chart from 'chart.js'
 import 'whatwg-fetch';
 
-React;
-
-export function scatterPlotClient(container) {
+export function scatterPlotClient(ctx) {
     
     fetch('/data')
         .then(function(response) {
             return response.json();
         })
-        .then(function(json) {
-
-            const data = json;
-            
-            ReactDOM.render(
-                <ScatterChart width={1000} height={500} margin={{ top: 20, right: 20, bottom: 10, left: 10 }}>
-                    <XAxis dataKey="x" name="stature" unit="cm" />
-                    <YAxis dataKey="y" name="weight" unit="kg" />
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-                    <Legend />
-                    <Scatter name="A school" data={data} fill="#8884d8" />
-                </ScatterChart>,
-                container
-            )
+        .then(function(data) {
+            const scatterChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    datasets: [{
+                        label: 'Scatter Dataset',
+                        data: data
+                    }]
+                },
+                options: {
+                    showLines: false,
+                    tooltips: {
+                        enabled: false
+                    },
+                    scales: {
+                        xAxes: [{
+                            type: 'linear',
+                            position: 'bottom'
+                        }]
+                    }
+                }
+            });
         })
         .catch(function(ex) {
             console.log('parsing failed', ex)
