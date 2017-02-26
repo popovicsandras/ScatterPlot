e51a7c68dd27d3aa8f94cdaf0ed3150e3f105c03
Andras Popovics <popovics@ndras.hu>
2017-02-26 13:24:40 +0000

    Mocha and babel impovements.

--- test/client/DataStore.spec.js
+++ test/client/DataStore.spec.js
@@ -1,9 +1,10 @@
 import assert from 'assert';
-// import DataStore from '../../src/client/DataStore';
+import DataStore from 'client/DataStore';
 
 describe('DataStore', function() {
 
     it('should', function() {
+        DataStore;
         assert.equal(true, true);
     });
 });
