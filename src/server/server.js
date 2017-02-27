f4b3fd05cf0f44a13a96e196dec959c13e184c9c
Andras Popovics <popovics@ndras.hu>
2017-02-27 18:38:19 +0000

    nvmrc and colorized winston.

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,10 @@
 const express = require("express"),
-    logger = require('winston'),
+    winston = require('winston'),
     dataEndpoint = require('./dataEndpoint');
+
+const logger = new (winston.Logger)({
+  transports: [ new (winston.transports.Console)({ colorize: true }) ]
+});
 
 function scatterPlotServer({publicPath, port, dataFilePath}) {
     const app = express();
@@ -8,7 +12,9 @@ function scatterPlotServer({publicPath, port, dataFilePath}) {
     app.use(express.static(publicPath));    
     app.get('/api/v1/data', dataEndpoint(dataFilePath, logger));
 
-    app.listen(port);
+    app.listen(port, () => {
+        logger.info(`Server started on port: http://localhost:${port}`);
+    });
 }
 
 module.exports = scatterPlotServer;
